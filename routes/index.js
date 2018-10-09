a33bfde58e9a4c418eb44d648d53ab1759b4c347
tjwhm <759361109@qq.com>
2018-10-09 16:46:14 +0800

    added ic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+
 var express = require('express');
+const path = require("path");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome' });
+router.get('/', function (req, res, next) {
+    res.sendFile(path.resolve(__dirname,'../public/welcome.html'));
+});
+
+router.get('/assets/ic_code.png', function (req, res, next) {
+    res.sendFile(path.resolve(__dirname,'../public/assets/ic_code.png'));
 });
 
 module.exports = router;
