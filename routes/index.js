959a664dd0e9fb9962cb9e294bfa0d7864d1333f
tjwhm <759361109@qq.com>
2018-10-11 02:53:44 +0800

    added jenkins 404

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.sendFile(path.resolve(__dirname,'../public/welcome.html'));
+    res.sendFile(path.resolve(__dirname,'../public/htmls/welcome.html'));
 });
 
 router.get('/assets/ic_code.png', function (req, res, next) {
     res.sendFile(path.resolve(__dirname,'../public/assets/ic_code.png'));
+});
+
+router.get('/jenkins_404', function (req, res, next) {
+    res.sendFile(path.resolve(__dirname,'../public/htmls/404/jenkins_404.html'));
 });
 
 module.exports = router;
