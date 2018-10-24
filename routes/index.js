5457c59d5d7090ec42d581cb5ea1f3516dd4954b
tjwhm <759361109@qq.com>
2018-10-25 02:09:23 +0800

    node 学艺不精，先把 JS 都写进一个文件

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
-
 var express = require('express');
 const path = require("path");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.sendFile(path.resolve(__dirname,'../public/htmls/welcome.html'));
+    res.sendFile(path.resolve(__dirname, '../public/htmls/welcome.html'));
 });
 
 router.get('/assets/ic_code.png', function (req, res, next) {
-    res.sendFile(path.resolve(__dirname,'../public/assets/ic_code.png'));
+    res.sendFile(path.resolve(__dirname, '../public/assets/ic_code.png'));
 });
 
 router.get('/jenkins_404', function (req, res, next) {
-    res.sendFile(path.resolve(__dirname,'../public/htmls/404/jenkins_404.html'));
+    res.sendFile(path.resolve(__dirname, '../public/htmls/404/jenkins_404.html'));
 });
 
 router.get('/puzzle', function (req, res, next) {
